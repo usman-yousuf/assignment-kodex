3c539280d4f1883a182806d78ce37918cbe1e9b4
Wahid Ahmad <wahid.kodex@gmail.com>
2021-10-04 19:10:18 +0500

    Today's updated code

---
+++ wahid/GoogleResultPage/04Oct2021/assets/script/script.js
@@ -0,0 +1,11 @@
+// Image Page Scroll Function 
+
+
+
+// Pseudo Code 
+
+// const button = document.getElementById('scroll_toggler-d');
+
+// button.onclick = function () {
+//   document.getElementById('container').scrollLeft += 20;
+// };
